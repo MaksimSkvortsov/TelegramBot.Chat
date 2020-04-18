eb94f719fe963479c439c73cc246748e2a4e3c35
mskvortsov <mskvortsov@acr.org>
2020-04-18 17:07:15 -0400

    Add input/output message DTOs to contracts project

---
+++ src/TelegramBot.Chat.Contract/ReplyQueueMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace TelegramBot.Chat.Contract
+{
+    public class ReplyQueueMessage
+    {
+        public string ChatId { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ src/TelegramBot.Chat.Contract/UpdateQueueMessage.cs
@@ -0,0 +1,19 @@
+﻿namespace TelegramBot.Chat.Contract
+{
+    public class UpdateQueueMessage
+    {
+        public string ChatId { get; set; }
+
+        public string UserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Message { get; set; }
+
+        public string LanguageCode { get; set; }
+
+        public bool IsBot { get; set; }
+    }
+}
